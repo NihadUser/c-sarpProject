cde6a266149f4aa8eee03e259ba2ae95915991bc
NihadUser <kermovnihad.com>
2024-11-30 11:26:26 +0400

    ado.net done

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new System.Windows.Forms.TextBox();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            loginBtn = new System.Windows.Forms.Button();
+            textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.AccessibleName = "";
+            textBox1.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            textBox1.Location = new System.Drawing.Point(107, 233);
+            textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Enter your username";
+            textBox1.Size = new System.Drawing.Size(267, 32);
+            textBox1.TabIndex = 0;
+            //textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (System.Drawing.Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            pictureBox1.Location = new System.Drawing.Point(0, 0);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(497, 705);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // loginBtn
+            // 
+            loginBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            loginBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            loginBtn.FlatAppearance.BorderColor = System.Drawing.Color.DarkKhaki;
+            loginBtn.FlatAppearance.BorderSize = 0;
+            loginBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            loginBtn.ForeColor = System.Drawing.Color.Snow;
+            loginBtn.Location = new System.Drawing.Point(107, 375);
+            loginBtn.Margin = new System.Windows.Forms.Padding(0);
+            loginBtn.Name = "loginBtn";
+            loginBtn.Size = new System.Drawing.Size(267, 75);
+            loginBtn.TabIndex = 3;
+            loginBtn.Text = "Login";
+            loginBtn.UseVisualStyleBackColor = false;
+            loginBtn.Click += loginBtn_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.AccessibleName = "";
+            textBox2.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            textBox2.Location = new System.Drawing.Point(107, 292);
+            textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Enter your password";
+            textBox2.Size = new System.Drawing.Size(267, 32);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(497, 705);
+            Controls.Add(textBox2);
+            Controls.Add(loginBtn);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;  
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+        public string sqlConn()
+        {
+            return "Data Source=DESKTOP-10DFVI1;Initial Catalog=e_commerce;Integrated Security=True;Trust Server Certificate=True";
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            string conn = sqlConn();
+
+            string uName = textBox1.Text.Trim();
+            string psw = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(uName) || string.IsNullOrEmpty(psw))
+            {
+                MessageBox.Show("Enter name and password.");
+                return;
+            }
+
+            string query = "SELECT * FROM users WHERE user_name = @user_name AND password = @password";
+
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@user_name", uName);
+                        command.Parameters.AddWithValue("@password", psw);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                reader.Read();
+
+                                string userId = reader["id"]?.ToString() ?? "N/A";
+                                string userName = reader["user_name"]?.ToString() ?? "N/A";
+
+                                MessageBox.Show($"success! User id is: {userId}, user name is: {userName}");
+                            }
+                            else
+                            {
+                                MessageBox.Show("incorrect username or password.");
+                            }
+                        }
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    MessageBox.Show($"Error is : {sqlEx.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+    }
+}
